a5dabdad9d6b09cd460925c9fb46258ec89ea58d
Alen Pariyar <oct10.alenpariyar@gmail.com>
2024-03-11 06:40:00 +0545

    2024-11-03

--- src/component/Hooks/UseState.js
+++ src/component/Hooks/UseState.js
@@ -1,13 +1,33 @@
-import React, { useCallback } from "react";
+import React from "react";
 import GoBack from "../GoBack";
-import { useNavigate } from "react-router-dom";
+
 
 const UseState = () => {
+    const [isValue, setIsValue] = React.useState(0);
+    const [product, setProduct] = React.useState({
+      multiplicationOf: 1,
+      upcount : 1,
+    });
+
 
     document.title = "Hooks - useEffect";  
+    
     return (
       <div className="main-body">
         useState
+
+        <p>Clicked {isValue} times.</p>
+        <p>Multiplication of {product.multiplicationOf} x {product.upcount} = {product.upcount*product.multiplicationOf} </p>
+        
+        <button onClick={()=>setIsValue(parseInt(isValue)+1)}>➕</button>
+        <button onClick={()=>setIsValue((parseInt(isValue)>0)?isValue-1:0)}>➖</button>
+        <button onClick={()=>setIsValue((parseInt(0)))}>🔁</button>
+        <button onClick={()=>setProduct(
+          {
+            ...product,
+            upcount : (product.upcount<10)?(product.upcount+1):(product.upcount),
+          }
+        )}>Multiply by 1</button>
         <GoBack></GoBack>
       </div>
     );
